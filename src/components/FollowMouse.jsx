e010621f48549ec8e883520b1cbdddb65d6f884b
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-02-23 19:12:34 -0500

    add FollowMouse component to track and display mouse position

---
+++ src/components/FollowMouse.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+
+export const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {}, [enabled]);
+  return (
+    <>
+      <div
+        style={{
+          position: 'absolute',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguimiento de puntero
+      </button>
+    </>
+  );
+};
